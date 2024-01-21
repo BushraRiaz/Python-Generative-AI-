9999ee93cca1a656bf2f1c7e14ed27a1885822b3
BushraRiaz <bushicaptain786@gmail.com>
2024-01-21 19:43:03 +0500

    class1

---
+++ class1.py
@@ -0,0 +1,8 @@
+print("Hello World");
+name = "Bushra"
+print(name)
+print(type(name))
+print(id(name)) #physical address
+print(i for i in dir(name) if "__" not in i) #Methods(Action) $ Attributes(Properties)
+name : list[str] = ['a', 'b', 'c']
+name : tuple [str, int, float] = ("BSSE", 4, 7.9)
